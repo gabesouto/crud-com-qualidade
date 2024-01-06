65d3a10aff24301ebe08e67a61047997f6a5616d
gabriel <soutogabriel04@gmail.com>
2024-01-06 18:11:25 -0300

    Feat: update function

--- first-crud-vannila-javascript/core/crud.ts
+++ first-crud-vannila-javascript/core/crud.ts
@@ -1,17 +1,20 @@
+import { error } from 'console';
 import fs from 'fs';
-import { encode } from 'punycode';
+import { v4 as uuid } from 'uuid';
 
 const DB_FILE_PATH = './core/db';
 
 interface todo {
 date: string,
 content: string,
-done:boolean
+done:boolean,
+id:string
 }
 
-function create(content: string){
+function create(content: string):todo{
 
    const todo:todo = {
+   id:uuid(),
    date: new Date().toISOString(),
    content: content,
    done:false
@@ -26,7 +29,8 @@ function create(content: string){
       todos,
       dogs: [],
    }, null, 2));
-      return content;
+     
+   return todo;
 };
 
 
@@ -41,10 +45,40 @@ function read():Array<todo>{;
    return db.todos
 }
 
+function update(id:string, partialTodo:Partial<todo>){
+   let updatedTodo;
+   const todos = read();
+
+   todos.forEach((currentTodo) => {
+     const isUpdating = currentTodo.id === id;
+     if(isUpdating){
+      updatedTodo = Object.assign(currentTodo, partialTodo)
+     }
+
+   })
+
+   fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+      todos,
+   }, null, 2))
+
+   if(!update){
+      throw new Error('Please, provide another ID!')
+   }
+   
+   return updatedTodo
+}
+
 function clearDB (){
    fs.writeFileSync(DB_FILE_PATH, "")
 }
+
 clearDB()
-create("crud de qualidade")
-create("novo crud com mais qualidade")
+create("primeiro crud de qualidade")
+create(" segundo novo crud com mais qualidade")
+const thirdTodo =  create("terceiro novo crud com mais qualidade")
 console.log(read());
+
+update(thirdTodo.id, {
+   content: "22222222222222"
+})
+
